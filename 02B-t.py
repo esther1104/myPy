59ff67c5942f4971d031f6da56355e39bacac19b
esther Kim <esther4185@gmail.com>
2019-07-28 21:48:55 +0900

    first commit

---
+++ 02B-t.py
@@ -0,0 +1,18 @@
+import turtle as t
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+
+t.circle(90)
---
+++ 02C-tt.py
@@ -0,0 +1,22 @@
+import turtle as t
+t.color("red")
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+t.fd(100)
+t.lt(120)
+
+t.pensize(3)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+t.lt(90)
+
+t.color("blue")
+t.pensize(5)
+t.circle(90)
