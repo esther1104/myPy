2cb632122004cdc043bbe8fee426024367120641
esther Kim <esther4185@gmail.com>
2019-08-01 20:48:13 +0900

    yeah

---
+++ DAY 8/08C-calc.py
@@ -0,0 +1,7 @@
+x=input("12+23=")
+a=int(x)
+
+if a==12+23:
+    print("대단해요!")
+else:
+    print("바보?")
--- kimminkyu.py
+++
@@ -1,6 +0,0 @@
-import turtle as t
-
-t.pensize(5)
-t.lt(180)
-t.color("yellow")
-t.forward(500)
